49144327b80110657c3cd8223d359fdb87197172
PiyushPagar <piyushpagar79@gmail.com>
2022-09-25 11:55:59 +0530

    first changes

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      home: Material(
+        child:Center(
+          child:Container(
+            child: Text("welcome to first flutter app"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+ 
