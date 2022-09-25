dade2feed0dbe5850cafd15170771649d9d526a1
PiyushPagar <piyushpagar79@gmail.com>
2022-09-25 16:58:45 +0530

    day 3 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bringVegetables(thaila: true);
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
+  }
+
+  bringVegetables({int rupees = 100, required bool thaila}) {
+    //take cycle
+
+    //go to sector 3 market
   }
 }
 
